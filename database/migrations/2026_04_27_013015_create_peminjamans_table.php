a8f4cd38f01e9149dff2af45108f7257c8521166
deviraokta <devira221okta@gmail.com>
2026-04-27 08:56:54 +0700

    migrasi database

---
+++ database/migrations/2026_04_27_013015_create_peminjamans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peminjamans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('buku_id')->constrained()->onDelete('cascade');
+            $table->date('tanggal_peminjaman');
+            $table->date('tanggal_jatuh_tempo');
+            $table->date('tanggal_pengembalian')->nullable();
+            $table->enum('status', ['dipinjam', 'dikembalikan'])->default('dipinjam');   
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjams');
+    }
+};
